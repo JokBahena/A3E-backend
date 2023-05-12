f9d57ba993eea95fc7e307cf78befb31af15d4df
JokBahena <joksan.bahena@a3e.com.mx>
2023-05-12 12:14:49 -0600

    Creacion de la app.js y db.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const db = require(".src/config/db")
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => {
+    console.log(err).stack;
+    res.status(500).send("Error interno en el servidor");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
+});
