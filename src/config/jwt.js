408cc7585f8c8b73dd3ef8408e88d73ede993124
emilianoa3e <emiliano.ortiz@a3e.com.mx>
2023-05-24 12:42:34 -0600

    change at jwt, one variable name corrected

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,12 +1,13 @@
-const jtw = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 //Function to generate token
 const generateToken = (payload) => {
-  return jtw.sign(payload, process.env.SECRET, { expiresIn: "1h" });
+  return jwt.sign(payload, process.env.SECRET, { expiresIn: "1h" });
 };
 
 //Export function
 module.exports = {
   generateToken,
 };
+
