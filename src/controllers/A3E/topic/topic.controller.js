9f9849198c7ae30bd810a5a41939cc9cbcb318de
JokBahena <joksan.bahena@a3e.com.mx>
2023-06-05 12:43:28 -0600

    xdddd

--- src/controllers/A3E/topic/topic.controller.js
+++ src/controllers/A3E/topic/topic.controller.js
@@ -1,5 +1,5 @@
 const { Response, Router } = require("express");
-const { save } = require("./topic.gateway");
+const { save, findAll, findById } = require("./topic.gateway");
 
 //Function to save and send data
 const saveAndFlush = async (req, res = Response) => {
@@ -21,11 +21,50 @@ const saveAndFlush = async (req, res = Response) => {
   }
 };
 
+//Function to get all topics
+const getAll = async (req, res = Response) => {
+  try {
+    //Call function to get all topics
+    const topics = await findAll();
+
+    //If topics exists
+    if (!topics) return res.status(400).json({ msg: "Topics not found" });
+    return res.status(200).json({ topics });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "Error getting topics",
+    });
+  }
+};
+
+//Function to get topic by id
+const getById = async (req, res = Response) => {
+  try {
+    //Extract id from params
+    const { id } = req.params;
+
+    //Call function to get topic by id
+    const topic = await findById(id);
+
+    //If topic exists
+    if (topic.msg) return res.status(400).json({ msg: topic.msg });
+    return res.status(200).json({ topic });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "Error getting topic",
+    });
+  }
+};
+
 //Route to save data
 const topicRouter = Router();
 
 //Define route
 topicRouter.post("/create-topic", [], saveAndFlush);
+topicRouter.get("/getAll-topics", [], getAll);
+topicRouter.get("/getById-topic/:id", [], getById);
 
 //Export route
 module.exports = { topicRouter };
--- src/controllers/A3E/topic/topic.gateway.js
+++ src/controllers/A3E/topic/topic.gateway.js
@@ -24,5 +24,29 @@ const save = async (nameSection, topics) => {
   }
 };
 
+//Function to get all topics
+const findAll = async () => {
+  try {
+    //Get all topics
+    return await Topic.find().select("-_id nameSection topics");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Function to get topic by id
+const findById = async (id) => {
+  try {
+    //Get topic by id
+    const topic = await Topic.findById(id).select("-_id nameSection topics");
+
+    //If topic exists
+    if (!topic) return { msg: "Topic not found" };
+    return topic;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 //Export function
-module.exports = { save };
+module.exports = { save, findAll, findById };
